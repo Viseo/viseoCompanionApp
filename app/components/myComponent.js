0b8b8fc00f2041ddd1c64c12f34210f4b691b2e7
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-02-24 13:10:08 +0100

    [Tech001] Test Env

---
+++ app/components/myComponent.js
@@ -0,0 +1,26 @@
+/**
+ * Created by LMA3606 on 24/02/2017.
+ */
+
+import React, {
+    StyleSheet,
+    PropTypes,
+    View,
+    Text,
+} from 'react-native';
+
+export default class MyComponent extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>I wonder if there will be any problems...</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
