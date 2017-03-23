e3d36e705839c5aca854e1dfafc9ef9c415f16ad
victoria <boucher.victoria@gmail.com>
2017-03-23 15:18:39 +0100

    [EventDetails CSS] Creation of eventDetailsParticipationInfos component

---
+++ app/components/events/eventDetailsParticipationInfos.js
@@ -0,0 +1,144 @@
+/**
+ * Created by VBO3596 on 22/03/2017.
+ */
+import React, {Component} from 'react';
+import {
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import AppText from '../appText';
+import strings from "../../util/localizedStrings";
+import * as util from '../../util/util';
+
+let {
+    height: deviceHeight,
+    width: deviceWidth
+} = Dimensions.get('window');
+
+class EventDetailsParticipationInfos extends Component {
+
+    static defaultProps = {
+        numberOfParticipants: '121',
+        notGoingColor: 'grey',
+        goingColor: 'royalblue'
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            going: this.props.event.participating
+        };
+    }
+
+    pressGoing = () =>{
+        this.setState({going: !this.state.going});
+        this.props.onPressGoing();
+    }
+
+    render() {
+        return (
+            <View style={styles.rectangle}>
+                <View style={styles.container}>
+                    {this.renderParticipants()}
+                    {this.renderDate(this.props.event.date)}
+                    {this.renderGoing()}
+                </View>
+            </View>
+        )
+    }
+
+    renderParticipants() {
+        return (
+            <View style={styles.infoItem}>
+                <AppText style={styles.main}>
+                    {this.props.numberOfParticipants}
+                </AppText>
+                <AppText style={styles.secondary}>
+                    {strings.participantsLabel}
+                </AppText>
+            </View>
+        );
+    }
+
+    renderDate(date) {
+        return (
+            <View style={styles.infoItem}>
+                <AppText style={styles.main}>
+                    {util.getFormattedHour(date)}
+                </AppText>
+                <AppText style={styles.secondary}>
+                    {this.formatDate(date)}
+                </AppText>
+            </View>
+        );
+    }
+
+    formatDate(date) {
+        return util.isDateInThisWeekNextDays(date) ? util.getDayName(date) : util.getFormattedDate(date);
+    }
+
+    renderGoing() {
+        let { going } = this.state;
+        return (
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <TouchableOpacity
+                    style={[styles.circle,
+                            going && {backgroundColor: this.props.goingColor},
+                            !going && {backgroundColor: this.props.notGoingColor}]}
+                    onPress={this.pressGoing}/>
+                <AppText>Going</AppText>
+            </View>
+        );
+    }
+}
+
+export default EventDetailsParticipationInfos;
+
+const styles = StyleSheet.create({
+    rectangle: {
+        height: deviceHeight * 0.1,
+        width: deviceWidth * 0.85,
+        borderRadius: 10,
+        borderWidth: 1,
+        backgroundColor: 'white',
+        borderColor: 'grey',
+    },
+
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginLeft: deviceWidth * 0.05,
+        marginRight: deviceWidth * 0.05,
+        marginTop: deviceWidth * 0.05,
+        marginBottom: deviceWidth * 0.05
+    },
+
+    infoItem: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    main: {
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 18
+    },
+
+    secondary: {
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 16
+    },
+
+    circle: {
+        height: 20,
+        width: 20,
+        borderRadius: 10,
+        margin: 2,
+    }
+});
