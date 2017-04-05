781d140eca85bfa2d33ccbb3939a8ae424731a4b
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-04-05 09:02:28 +0200

    PushController file added

---
+++ app/util/pushController.js
@@ -0,0 +1,49 @@
+/**
+ * Created by LMA3606 on 04/04/2017.
+ */
+
+import React, {Component} from 'react';
+import PushNotification from 'react-native-push-notification';
+
+export default class PushController extends Component {
+    componentDidMount() {
+        PushNotification.configure({
+
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function (token) {
+                console.log('TOKEN:', token);
+            },
+
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: function (notification) {
+                console.log('NOTIFICATION:', notification);
+            },
+
+            // ANDROID ONLY: (optional) GCM Sender ID.
+            senderID: "YOUR GCM SENDER ID",
+
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+
+            /**
+             * (optional) default: true
+             * - Specified if permissions (ios) and token (android and ios) will requested or not,
+             * - if not, you must call PushNotificationsHandler.requestPermissions() later
+             */
+            requestPermissions: true,
+        });
+    }
+
+    render() {
+        return null;
+    }
+}
+
