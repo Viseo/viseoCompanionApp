a3e1060eb78e78a4331b2e998ad25fe1e8dce731
alexandre abrantes <alexandre.abts@gmail.com>
2017-04-18 17:01:14 +0200

    Refactored App Text

--- app/__tests__/appText.spec.js
+++ app/__tests__/appText.spec.js
@@ -1,21 +1,15 @@
-/**
- * Created by LMA3606 on 21/03/2017.
- */
-
-import React from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-} from "react-native";
-import testUtil from './testUtil';
+    createComponent,
+    compareComponentChildren
+} from './TestUtils';
 
 describe('AppText', () => {
-    it('should diplay the test passed in his props', () => {
-        const appText = testUtil.createAppText({
-            className: "errorText",
-            children: "Big big error",
-        });
-        testUtil.checkFieldContent(appText, "errorText", "Big big error")
+
+    it('should display the test passed in props', () => {
+        const appText = createComponent('AppText', {
+            children: 'Some lovely text'
+        })
+        compareComponentChildren(appText, 'Some lovely text')
     });
+
 });
--- app/__tests__/testComponents.js
+++ app/__tests__/testComponents.js
@@ -1,37 +1,18 @@
-/**
- * Created by AAB3605 on 20/03/2017.
- */
 import React from "react";
 import {shallow, mount} from "enzyme";
-import EventListView from '../components/events/eventListView';
 import EventCard from '../components/events/eventCard';
-import EventDetails from '../components/events/eventDetails';
-import Filter from '../components/events/filter';
-import FilterBar from '../components/events/filterBar';
 import Header from '../components/header';
 import ViseoCompanion from '../index';
 import Home from '../scenes/home';
 import EditableImage from '../components/editableImage';
+import AppText from './../components/appText'
 
-function get(componentName, props) {
+export function get(componentName, props) {
     switch (componentName) {
+        case 'AppText':
+            return shallow(<AppText {...props}/>)
         case 'EventCard':
             return shallow(<EventCard {...props}/>);
-            break;
-        case 'EventDetails':
-            return shallow(<EventDetails {...props}/>);
-            break;
-        case 'EventListView':
-            return shallow(<EventListView {...props}/>);
-            break;
-        case 'Filter':
-            return shallow(<Filter {...props}/>);
-            break;
-        case 'FilterBar':
-            return shallow(<FilterBar {...props}/>);
-            break;
-        case 'Header':
-            return shallow(<Header {...props}/>);
             break;
         case 'Home':
             return shallow(<Home {...props}/>);
