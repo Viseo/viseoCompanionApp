8f34c01c4c76eb21d2f607fc26259f2da11c7041
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2017-04-26 11:58:31 +0200

    [pushNotification] schedule local notification iOS

--- app/__tests__/PushController.spec.js
+++ app/__tests__/PushController.spec.js
@@ -1,17 +1,19 @@
 import {checkTestFunction, createComponent, createTestFunction} from "./TestUtil/";
 
 describe('PushController', () => {
-
-    it('should function when the component is instantiate', () => {
-        const requestPermissions = createTestFunction();
-        const getFCMToken = createTestFunction();
-        const subscribeToTopic = createTestFunction();
+//todo understand why the function are not called (testing method must be wrong!)
+    it('should function when the component is instantiated', () => {
+        const FCM = {
+            requestPermissions : createTestFunction(),
+            getFCMToken : createTestFunction(),
+            subscribeToTopic : createTestFunction()
+        }
 
         const pushController = createComponent("pushController")
 
-        checkTestFunction(requestPermissions);
-        checkTestFunction(getFCMToken);
-        checkTestFunction(subscribeToTopic);
+        checkTestFunction(FCM.requestPermissions);
+        checkTestFunction(FCM.getFCMToken);
+        checkTestFunction(FCM.subscribeToTopic);
     })
 
 });
