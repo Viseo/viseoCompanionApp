ad686f939258ba0225d887fa5529532a4eea77e9
Alexandre <alexandre@Alexandres-MacBook-Pro.local>
2017-05-31 17:05:26 +0200

    created websocket file

---
+++ app/modules/live/configureWebSocket.js
@@ -0,0 +1,24 @@
+import {serverURL} from './../global/settings';
+
+let ws = new WebSocket('ws://' + serverURL + 'liveEvent');
+
+ws.onopen = () => {
+    // connection opened
+
+    ws.send('something'); // send a message
+};
+
+ws.onmessage = (e) => {
+    // a message was received
+    console.log(e.data);
+};
+
+ws.onerror = (e) => {
+    // an error occurred
+    console.log(e.message);
+};
+
+ws.onclose = (e) => {
+    // connection closed
+    console.log(e.code, e.reason);
+};
