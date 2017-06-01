5f893472660910ac1374e4897bc19dc2bbca5952
MBE3664 <mohamedaziz.benmiled@gmail.com>
2017-06-01 16:21:25 +0200

    ChildComment fixed

--- app/modules/events/registerScreens.js
+++ app/modules/events/registerScreens.js
@@ -4,6 +4,10 @@ import PastEvents from "./PastEvents";
 import PastEvent from "./PastEvent";
 import CreateEvent from "./CreateEvent";
 import Event from './Event';
+import Comments from "../../scenes/commentsList";
+import CreateComment from "./CreateComment";
+import CreateChildComment from "./CreateChildComment";
+//import UpdateComment from "./UpdateComment";
 
 export default function(store, provider) {
     Navigation.registerComponent('events.event', () => Event, store, provider);
@@ -11,4 +15,8 @@ export default function(store, provider) {
     Navigation.registerComponent('events.pastEvents', () => PastEvents, store, provider);
     Navigation.registerComponent('events.pastEvent', () => PastEvent, store, provider);
     Navigation.registerComponent('CreateEvent', () => CreateEvent, store, provider);
+    Navigation.registerComponent('Comments', () => Comments, store, provider);
+    Navigation.registerComponent('CreateComment', () => CreateComment, store, provider);
+    Navigation.registerComponent('CreateChildComment', () => CreateChildComment, store, provider);
+    //Navigation.registerComponent('UpdateComment', () => UpdateComment, store, provider);
 }
--- app/modules/global/settings.js
+++ app/modules/global/settings.js
@@ -1,7 +1,7 @@
 /////////// SERVER CONNECTION ////////////////
 
 // Localhost (set your localhost IP here)
-const localhostIp = '10.33.171.57';
+const localhostIp = '10.33.179.5';
 const localhostURL = 'http://' + localhostIp + ':8080/';
 
 // The server URL, you usually shouldn't have to change this
@@ -20,6 +20,14 @@ const restRoutes = {
     getUser: serverURL + 'users/',
     authenticate: serverURL + 'authenticate',
     resetPassword: serverURL + 'resetPassword',
+    addChildComment: serverURL + 'comments/2',
+    /*addChildComment: (commentId) => {
+     return SERVER_API_URL + 'comments/' + commentId ;
+     },*/
+    getCommentsByEvent: (eventId) => {
+        return serverURL + 'comments/events/' + eventId ;
+    },
+    updatedComment: serverURL + 'comments',
     addEventParticipant: (eventId, userId) => {
         return serverURL + 'events/' + eventId + '/users/' + userId;
     },
@@ -28,6 +36,18 @@ const restRoutes = {
     },
     getEventParticipants: eventId => {
         return serverURL + 'events/' + eventId + '/users';
+    },
+    getCommentsByEvent: (eventId) => {
+        return serverURL + 'comments/events/' + eventId ;
+    },
+    likeComment:(commentId,userId)=> {
+        return serverURL + 'comments/'+commentId+'/like/'+userId;
+    },
+    dislikeComment:(commentId,userId)=> {
+        return serverURL + 'comments/'+commentId+'/like/'+userId;
+    },
+    deleteComment:(commentId)=> {
+        return serverURL + 'comments/'+commentId;
     },
     updatedEvent: eventId => {
         return serverURL + 'events/' + eventId;
